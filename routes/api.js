2b1b040649242ef870f95e3b041b7125d243d066
Guo Yu <o.u.turing@gmail.com>
2016-01-19 21:40:17 +0800

    Add LeanEngine cloud function test route

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,19 @@
 'use strict';
 
 import express from 'express'
+import { Cloud } from 'leanengine'
 
 const API = express.Router()
 
+// a test route
 API.get('/ping', (req, res, next) => res.end('pong'))
+
+// a example for LeanEngine Cloud function
+API.get('/hello', (req, res, next) => {
+  Cloud.run('hello', {}, {
+    success: data => res.end(data),
+    error: err => next(err)
+  })
+})
 
 export default API
